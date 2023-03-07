bb992c0ebf072621f76aaeb0d8759dacec7f265e
DmitryKnyazkov <kde.2015@yandex.ru>
2023-03-07 18:11:28 +0300

    Лаба 3. Быстрая сортировка

---
+++ Сортировки/Практические задания/Быстрая сортировка/task.py
@@ -0,0 +1,28 @@
+from typing import List
+
+def sort(container: List[int]) -> List[int]:
+    """
+    Алгоритм быстрой сортировки.
+
+    1. Выбираем опорный элемент. Например, первый элемент.
+    2. В левую часть отправляем всё что меньше опорного элемента, в правую всё что больше.
+    3. К левой и правой части рекурсивно применяет алгоритм быстрой сортировки.
+
+    :param container: последовательность, которую надо отсортировать
+    :return: Отсортированная в порядке возрастания последовательность
+    """
+    if not container:
+        return container
+
+    pivot = container[0]
+    return (
+            sort([item for item in container if item < pivot]) +
+            [item for item in container if item == pivot] +
+            sort([item for item in container if item > pivot])
+    )
+
+
+a = [5,4,3,2,1]
+print(sort(a))
+
+      # TODO реализовать алгоритм быстрой сортировки
